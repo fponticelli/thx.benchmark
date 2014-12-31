203d2bfd757e0619e7b8e7732a16ea15bed09c2e
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-12-31 14:52:45 -0700

    updated doc files

--- doc/ImportBenchmark.hx
+++ doc/ImportBenchmark.hx
@@ -1,5 +1,7 @@
 import thx.benchmark.speed.ISpeedCase;
 import thx.benchmark.speed.SpeedCase;
 import thx.benchmark.speed.SpeedResult;
+import thx.benchmark.speed.SpeedSuite;
 import thx.benchmark.speed.SpeedSummary;
 import thx.benchmark.speed.SpeedTest;
+import thx.benchmark.speed.SpeedTestSummary;
