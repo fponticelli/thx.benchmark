daf767c148d1860375e7bb5162710c9726c742c4
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-12-30 17:07:29 -0700

    added function to generate better string result

--- src/thx/benchmark/speed/SpeedResult.hx
+++ src/thx/benchmark/speed/SpeedResult.hx
@@ -1,6 +1,7 @@
 package thx.benchmark.speed;
 
 using thx.core.Floats;
+using thx.core.Strings;
 
 class SpeedResult {
   public var index : Int;
@@ -14,6 +15,20 @@ class SpeedResult {
   };
 
   public function toString() {
-    return '$description${isReference ? " (reference)" : ""}: ${time.roundTo(3)}ms';
+    return '${isReference ? "* " : "  "} $description: ${time.roundTo(3)}ms';
+  }
+
+  public function toStringComparison(descriptionLen : Int, reference : Float) {
+    var des = description.rpad(".", descriptionLen),
+        round = 2,
+        per = ""+((reference / time) * 100).roundTo(round),
+        parts = per.split(".");
+    if(1 == parts.length)
+      parts[1] = "0".repeat(round);
+    else
+      parts[1] = parts[1].rpad("0", round);
+
+    per = parts.join(".");
+    return '${isReference ? "* " : "  "}$des..: ${per.lpad(" ", 8)}% (${time.roundTo(3)}ms)';
   }
 }
