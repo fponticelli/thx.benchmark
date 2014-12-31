5f99d52b5e560e92074ce87a839c43d46300f386
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-12-30 18:41:17 -0700

    minor

--- src/thx/benchmark/speed/SpeedSuiteSummary.hx
+++ src/thx/benchmark/speed/SpeedSuiteSummary.hx
@@ -13,9 +13,10 @@ class SpeedSuiteSummary {
   public function toString() {
     var sub = "=".repeat(list.reduce(function(acc, item) {
                 return item.description.length.max(acc);
-              }, 0));
+              }, 0) + 2);
     return list.map(function(item) {
-      return '${item.description}
+      return '
+${item.description}
 $sub
 
 ${indent(item.summary.toString().trim())}
