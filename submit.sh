28f70bfad9615b5f35e5de91a21eb6e670f78598
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 20:11:00 -0600

    exclude hidden files from zip

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm thx.benchmark.zip
-zip -r thx.benchmark.zip hxml src test doc/ImportBenchmark.hx extraParams.hxml haxelib.json README.md
+zip -r thx.benchmark.zip hxml src test doc/ImportBenchmark.hx extraParams.hxml haxelib.json README.md -x "*/\.*"
 haxelib submit thx.benchmark.zip
