d41a84ef8bf86dd674a79a1d8f7bb641102a19c8
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-06-01 18:03:19 -0600

    minor

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm thx.benchmark.zip
-zip -r thx.benchmark.zip hxml src test doc/ImportBenchmark.hx extraParams.hxml haxelib.json README.md -x "*/\.*"
+zip -r thx.benchmark.zip hxml src test doc/ImportBenchmark.hx extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
 haxelib submit thx.benchmark.zip
