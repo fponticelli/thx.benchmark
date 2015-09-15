68ce44a05908e96906d93b369cae674ab72d7f86
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-09-14 21:09:57 -0600

    create test expression

--- src/thx/benchmark/test/macro/SpeedCaseBuilder.hx
+++ src/thx/benchmark/test/macro/SpeedCaseBuilder.hx
@@ -4,6 +4,7 @@ using thx.Arrays;
 using thx.macro.MacroFields;
 using thx.macro.MacroTypes;
 using thx.macro.MacroClassTypes;
+using thx.macro.MacroExprs;
 import haxe.macro.Type;
 import haxe.macro.Expr;
 import haxe.macro.Context;
@@ -22,7 +23,73 @@ class SpeedCaseBuilder {
     ensureEmptyFunction(cls, fields, "test");
     ensureEmptyFunction(cls, fields, "teardown");
 
+    // construct sub-type
+    constructSubType(cls, fields);
     return fields;
+  }
+
+  static function extractExpression(kind : FieldType) {
+    return switch kind {
+      case FVar(_, e): e;
+      case FFun(f): f.expr;
+      case FProp(_, _, _, e): e;
+    };
+  }
+
+  static function constructSubType(cls : ClassType, fields : Array<Field>) {
+    var description = fields.find(function(field) return field.name == "description");
+    $type(description.kind);
+    var descriptionExpression = extractExpression(description.kind);
+    var setup = fields.find(function(field) return field.name == "setup");
+    var setupExpression = extractExpression(setup.kind);
+    var teardown = fields.find(function(field) return field.name == "teardown");
+    var teardownExpression = extractExpression(teardown.kind);
+    var test = fields.find(function(field) return field.name == "test");
+    var testExpression = extractExpression(test.kind);
+
+    var loopExpression = macro {
+      var start, end, span;
+      start = thx.Timer.time();
+      while(--loop >= 0)
+        $e{testExpression}
+      end = thx.Timer.time();
+      span = end - start;
+    };
+    var closingExpression = macro return span;
+    var testSpeedExpressions = [
+          setupExpression.unwrapBlock(),
+          loopExpression.unwrapBlock(),
+          teardownExpression.unwrapBlock(),
+          [closingExpression]
+        ].flatten();
+    trace(haxe.macro.ExprTools.toString(macro $b{testSpeedExpressions}));
+    var fields = [{
+      pos: cls.pos,
+      name: "description",
+      kind: FVar(macro : String, descriptionExpression),
+      access: [APublic],
+    }, {
+      pos: cls.pos,
+      name: "testSpeed",
+      kind: FFun({
+        ret : macro : Float,
+        expr : macro $b{testSpeedExpressions},
+        args : [{name : "counter", type : macro : Int}]
+      }),
+      access: [APublic],
+    }];
+    var interfaces = [{ pack : ["thx","benchmark","test"], name : "ISpeedTest" }];
+    var typeDefinition = {
+      pos: cls.pos,
+      params: null,
+      pack: cls.pack,
+      name: '${cls.name}_SpeedTest',
+      meta: null,
+      kind: TDClass(null, interfaces, false),
+      isExtern: false,
+      fields: fields
+    };
+    Context.defineType(typeDefinition);
   }
 
   static function ensureDescription(cls : ClassType, fields : Array<Field>) {
--- test/ForVsMap.hx
+++ test/ForVsMap.hx
@@ -1,30 +1,47 @@
 using thx.Arrays;
 
-class For extends BaseForVsMap implements thx.benchmark.test.ISpeedCase {
+class For implements thx.benchmark.test.ISpeedCase {
   function test() {
     var result = 0;
     for(v in arr)
       result += v;
     value = result;
   }
+  var arr : Array<Int>;
+  var value : Int;
+  public function setup() {
+    arr = [for(i in 0...10000) i % 100];
+  }
+
+  public function teardown() {
+    if(value < 0)
+      throw 'invalid value returned';
+  }
 }
 
-class TestMap extends BaseForVsMap implements thx.benchmark.test.ISpeedCase {
+class TestMap implements thx.benchmark.test.ISpeedCase {
   var description = "map";
   function test() {
     var result = 0;
     arr.map(function(v) result += v);
     value = result;
   }
-}
+  var arr : Array<Int>;
+  var value : Int;
+  public function setup() {
+    arr = [for(i in 0...10000) i % 100];
+  }
 
-class Reduce extends BaseForVsMap implements thx.benchmark.test.ISpeedCase {
-  function test() {
-    value = arr.reduce(function(acc, v) return acc + v, 0);
+  public function teardown() {
+    if(value < 0)
+      throw 'invalid value returned';
   }
 }
 
-class BaseForVsMap {
+class Reduce implements thx.benchmark.test.ISpeedCase {
+  function test() {
+    value = arr.reduce(function(acc, v) return acc + v, 0);
+  }
   var arr : Array<Int>;
   var value : Int;
   public function setup() {
